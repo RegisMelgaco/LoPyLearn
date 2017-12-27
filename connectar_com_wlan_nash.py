fd35f6532cae0cfb49e7802e4b6ab486eb801ede
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2017-12-27 14:38:57 -0300

    alteração do ip fixo para um valido

--- connectar_com_wlan_nash.py
+++ connectar_com_wlan_nash.py
@@ -8,12 +8,11 @@ for net in nets:
         print('Network found!')
         wlan.connect(net.ssid, auth=(net.sec, 'nashifce8556'), timeout=5000)
         while not wlan.isconnected():
-            machine.idle() # save power while waiting
+            machine.idle()
         print('WLAN connection succeeded!')
         break
 
 wlan.init(mode=WLAN.STA)
-# configuration below MUST match your home router settings!!
-wlan.ifconfig(config=('192.168.178.117', '255.255.255.0', '192.168.178.1', '8.8.8.8'))
+wlan.ifconfig(config=('192.168.0.3', '255.255.255.0', '192.168.178.1', '8.8.8.8'))
 
 print("ready!")
