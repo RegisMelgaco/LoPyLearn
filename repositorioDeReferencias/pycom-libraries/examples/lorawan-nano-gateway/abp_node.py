59506224d4bf40d7f4a8ed6340d5af4667f327a8
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2017-12-27 14:41:25 -0300

    repositorio de referencias

---
+++ repositorioDeReferencias/pycom-libraries/examples/lorawan-nano-gateway/abp_node.py
@@ -0,0 +1,43 @@
+from network import LoRa
+import socket
+import binascii
+import struct
+import time
+import config
+
+# initialize LoRa in LORAWAN mode.
+lora = LoRa(mode=LoRa.LORAWAN)
+
+# create an ABP authentication params
+dev_addr = struct.unpack(">l", binascii.unhexlify('26 01 14 7D'.replace(' ','')))[0]
+nwk_swkey = binascii.unhexlify('3C 74 F4 F4 0C AE A0 21 30 3B C2 42 84 FC F3 AF'.replace(' ',''))
+app_swkey = binascii.unhexlify('0F FA 70 72 CC 6F F6 9A 10 2A 0F 39 BE B0 88 0F'.replace(' ',''))
+
+# remove all the non-default channels
+for i in range(3, 16):
+    lora.remove_channel(i)
+
+# set the 3 default channels to the same frequency
+lora.add_channel(0, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
+lora.add_channel(1, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
+lora.add_channel(2, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=5)
+
+# join a network using ABP (Activation By Personalization)
+lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, config.LORA_NODE_DR)
+
+# make the socket blocking
+s.setblocking(False)
+
+for i in range (200):
+    s.send(b'PKT #' + bytes([i]))
+    time.sleep(4)
+    rx, port = s.recvfrom(256)
+    if rx:
+        print('Received: {}, on port: {}'.format(rx, port))
+    time.sleep(6)
---
+++ repositorioDeReferencias/pycom-libraries/examples/lorawan-nano-gateway/otaa_node_US915.py
@@ -0,0 +1,59 @@
+""" OTAA Node example compatible with the LoPy Nano Gateway """
+
+from network import LoRa
+import socket
+import binascii
+import struct
+import time
+import config
+
+# initialize LoRa in LORAWAN mode.
+lora = LoRa(mode=LoRa.LORAWAN)
+
+# create an OTA authentication params
+dev_eui = binascii.unhexlify('AA BB CC DD EE FF 77 78'.replace(' ',''))
+app_eui = binascii.unhexlify('70 B3 D5 7E F0 00 3B FD'.replace(' ',''))
+app_key = binascii.unhexlify('36 AB 76 25 FE 77 0B 68 81 68 3B 49 53 00 FF D6'.replace(' ',''))
+
+# remove all the channels
+for channel in range(0, 72):
+    lora.remove_channel(channel)
+
+# set all channels to the same frequency (must be before sending the OTAA join request)
+for channel in range(0, 72):
+    lora.add_channel(channel, frequency=config.LORA_FREQUENCY, dr_min=0, dr_max=3)
+
+# join a network using OTAA
+lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0, dr=config.LORA_NODE_DR)
+
+# wait until the module has joined the network
+join_wait = 0
+while True: 
+    time.sleep(2.5)
+    if not lora.has_joined():
+        print('Not joined yet...')
+        join_wait += 1
+        if join_wait == 5:
+            lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0, dr=config.LORA_NODE_DR)
+            join_wait = 0            
+    else:
+        break
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, config.LORA_NODE_DR)
+
+# make the socket blocking
+s.setblocking(False)
+
+time.sleep(5.0)
+
+for i in range (200):
+    s.send(b'PKT #' + bytes([i]))
+    time.sleep(4)
+    rx, port = s.recvfrom(256)
+    if rx:
+        print('Received: {}, on port: {}'.format(rx, port))
+    time.sleep(6)
